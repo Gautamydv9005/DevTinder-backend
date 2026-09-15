9322629e649e3026d27bdeac22eafff4e1ac894a
Gautamydv9005 <yadavgautam9005@gmail.com>
2026-09-16 00:14:35 +0530

    passing dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -5,29 +5,23 @@ const port = 7777;
 const connectDB=  require("./config/DataBase");
 const User= require("./models/user");
 
-app.post("/signup",async(req,res)=>{
-    
-        const user=new User({
-        FirstName:"Govinda",
-        LastName:"Yadav",
-        Age:25,
-        Password:"Govinda@123",
-        Email:"govindayadav9005@gmail.com"
-    });
+app.use(express.json());
 
+
+app.post("/signup",async(req,res)=>{
+    const user=new User(req.body);
     try{
 
     await user.save();
     res.send("User data added successfully");
+
     }
     catch(err){
+        console.log(err);
         res.status(401).send("something went wrong");
     }
     
-})
-
-
-
+});
 
 connectDB()
 .then(()=>{
